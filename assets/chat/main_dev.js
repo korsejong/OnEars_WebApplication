f614e9d7a12e6b9716f49f8d9a0673559c1cb7ac
korsejong <sejong.400@gmail.com>
2018-10-14 17:57:52 +0900

    move database.js

--- assets/chat/main_dev.js
+++ assets/chat/main_dev.js
@@ -163,7 +163,6 @@ class UserMessageCreater extends MessageCreater {
         return userMessage;
 	}
 }
-
 class SpeechRecognition extends webkitSpeechRecognition {
     construct(language){
         this.continuous = false;
@@ -171,7 +170,6 @@ class SpeechRecognition extends webkitSpeechRecognition {
         this.lang = language;
     }
 }
-
 class PreviousMessageCaller {
     setUserId(userId){
         this._userId = userId;
@@ -196,17 +194,24 @@ class PreviousMessageCaller {
         
     }
 }
-
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+if (!window.indexedDB) {
+    // 브라우저 indexedDB 지원 체크
+    window.alert("Your browser doesn't support a stable version of IndexedDB.")
+}
+const dbName = "user";
+const indexdDB = window.indexedDB.open(dbName, 1);
 const API_KEY = "https://lu8da3cuo2.execute-api.ap-northeast-2.amazonaws.com/version1";
 const apigClient = apigClientFactory.newClient({apikey: API_KEY});
 const LANGUAGE = "ko-KR";
 const chatContainerElement = $('.chat_group')[0];
-
 const previousMessageCaller = new PreviousMessageCaller();
 const userMessageCreater = new UserMessageCreater(chatContainerElement);
 const serverMessageCreater = new ServerMessageCreater(chatContainerElement);
 const guideMessageCreater = new GuideMessageCreater(chatContainerElement);
-
+let db;
 let userId = '';
 let userName = '';
 let state = {
@@ -216,7 +221,6 @@ let state = {
     title: '',
     url: ''
 };
-
 let userMessages = [];
 let serverMessages = [];
 let userMessage = null;
@@ -399,6 +403,10 @@ const createGuideMessage = () => {
         guideMessage.playAudio();
     }
 };
+const openModal = () => {
+    $('#user_info_modal').modal({backdrop: 'static'});
+    $('#user_info_modal').modal();
+}
 
 // overlay functions
 const sizeTheOverlays = () => {
@@ -409,9 +417,7 @@ const sizeTheOverlays = () => {
     $(this).css("width", w);
   });
 };
-
 sizeTheOverlays();
-
 let width = $(window).width();
 $(window).resize(function(){
    if($(this).width() != width){
@@ -419,6 +425,92 @@ $(window).resize(function(){
       sizeTheOverlays();
    }
 });
+
+// indexedDB functions
+// indexedDB open async onerror, onsuccess
+indexdDB.onerror = (event) => {
+    console.log("indexedDB open error");
+};
+indexdDB.onsuccess = (event) => {
+    try{
+        db = indexdDB.result;
+        readAll();
+    }catch(e){
+        console.log(e);
+    }
+};
+indexdDB.onupgradeneeded = (event) => {
+    try{
+        db = event.target.result;
+        db.createObjectStore(dbName, { keyPath: "userId"} );
+    }catch(e){
+        console.log(e);
+    }
+};
+const read = (id) => {
+    try{
+        let objectStore = db.transaction(dbName).objectStore(dbName);
+        let request = objectStore.get(id);
+        request.onerror = (event) => {
+            console.log("log: read() onerror");
+        };
+        request.onsuccess = (event) => {
+            console.log(request.result);
+        };
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+const readAll = () => {
+    try{
+        let objectStore = db.transaction(dbName).objectStore(dbName);
+        let request = objectStore.getAll();
+        request.onerror = (event) => {
+            console.log("log: readAll() onerror");
+        };
+        request.onsuccess = (event) => {
+            console.log("log: readAll() onsuccess");
+            if(request.result.length == 0){
+                openModal();
+            }else{
+                userId = request.result[0].userId;
+                userName = request.result[0].userName;
+                connect();
+                serverMessages.push(guideMessageCreater.create({data:`환영합니다 ${userName}님! 잠시만 기다려주시면 메세지를 전달해 드릴께요 :)`}));
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+const add = (user) => {
+    try{
+        let request = db.transaction([dbName], "readwrite").objectStore(dbName).add(user);
+        request.onsuccess = (event) => {
+            console.log("log: add() onsuccess");
+        };
+        request.onerror = (event) => {
+            console.log("log: add() onerror");
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+const remove = (userId) => {
+    try{
+        let request = db.transaction([dbName], "readwrite").objectStore(dbName).delete(userId);
+        request.onsuccess = (event) => {
+            console.log("log: remove() onsuccess");
+        };
+        request.onerror = (event) => {
+            console.log("err: remove() onerror");
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+}
 
 // TEST FUNCTION
 const selectMessageTestFunction = (type,idx) => {
